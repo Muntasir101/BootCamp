cd10830b796ed8fa3ce774979a89605a98f37841
Muntasir Abdullah <muntasir.abdullah01@gmail.com>
2021-03-02 12:54:24 +0600

    implement CNN logout

--- src/test/java/com/CNN/CNN_Login.java
+++ src/test/java/com/CNN/CNN_Login.java
@@ -7,19 +7,27 @@ import org.testng.annotations.Test;
 
 public class CNN_Login extends TestBase {
     /*
-    mail22@mail.com
+    mail22@yopmail.com
     TSkfmF5D9Fsz@eL
      */
 
     @Test(priority = 0)
     public static void CNN_login() throws InterruptedException {
-        String email="mail22@mail.com";
+        String email="mail22@yopmail.com";
         String password="TSkfmF5D9Fsz@eL";
 
         driver.findElement(By.cssSelector(".Flex-sc-1sqrs56-0 .user-icon")).click();
         driver.findElement(By.name("loginEmail")).sendKeys(email);
-        driver.findElement(By.name("loginPassword")).sendKeys(password);
+        driver.findElement(By.name("loginPassword")).sendKeys("TSkfmF5D9Fsz@eL");
         driver.findElement(By.xpath("//*[@id=\"login-form-button\"]")).click();
 
+        Thread.sleep(5000);
+
+        // Log Out
+        driver.findElement(By.cssSelector("#account-header-button-desktop > div.Box-sc-1fet97o-0.hyVhvp")).click();
+        driver.findElement(By.linkText("Log Out")).click();
+
     }
+
+
 }
